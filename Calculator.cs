c8db4be0e703c05ccde778d6582b78a9f31db486
farhandanu12 <72136325+farhandanu12@users.noreply.github.com>
2021-10-01 11:03:05 +0700

    update

--- Calculator.cs
+++ Calculator.cs
@@ -2,13 +2,13 @@ static void Main(string[] args)
         {
             Console.Title = "Aplikasi Kalkulator";
 
-            int a = 10;
-            int b = 5;
+            int a = 20;
+            int b = 10;
 
-            Console.WriteLine("Hasil Penambahan " + a + " + " + b + " = " + Penambahan(a, b));
-            Console.WriteLine("Hasil Pengurangan {0} - {1} = {2}", a, b, Pengurangan(a, b));
-            Console.WriteLine("Hasil Pembagian {0} : {1} = {2}", a, b, Pembagian(a, b));
-            Console.WriteLine("Hasil Perkalian {0} x {1} = {2}", a, b, Perkalian(a, b));
+            Console.WriteLine("Hasil dari Penambahan " + a + " + " + b + " = " + Penambahan(a, b));
+            Console.WriteLine("Hasil dari Pengurangan {0} - {1} = {2}", a, b, Pengurangan(a, b));
+            Console.WriteLine("Hasil dari Pembagian {0} : {1} = {2}", a, b, Pembagian(a, b));
+            Console.WriteLine("Hasil dari Perkalian {0} x {1} = {2}", a, b, Perkalian(a, b));
 
             Console.WriteLine("\nTekan key sembarang untuk keluar...");
             Console.ReadKey();
